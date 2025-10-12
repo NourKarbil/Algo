5dc567281ad4e09c5be88a3686d014e93cbdd515
NourKarbil <nour.karbil@uha.fr>
2025-10-12 18:11:09 +0200

    Update main.c

---
+++ Algo TD 1/Exercice 2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include "Premier.h"  
+
+int main() {
+    int n = 100;  
+    int result;
+
+    clock_t start, end;
+    double cpu_time_used;
+
+    
+    // Premier Naive
+    start = clock();
+    result = premier_naive(n);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Resultat (naive) : %d\n", result);
+    printf("Temps CPU (naive) : %.6f secondes\n", cpu_time_used);
+
+  
+    // Premier Optimisé
+
+    start = clock();
+    result = premier_optimized(n);
+    end = clock();
+
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Resultat (optimise) : %d\n", result);
+    printf("Temps CPU (optimise) : %.6f secondes\n", cpu_time_used);
+    return 0;
+}
